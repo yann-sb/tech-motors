bfb8c7d15f303fe69fba345d59263153122ad018
yann-sb <107086412+yann-sb@users.noreply.github.com>
2022-09-20 08:55:43 -0300

    sql update

--- sql/server_tech_motors_V0,4.sql
+++ sql/server_tech_motors_V0,4.sql
@@ -1,21 +1,21 @@
-/*
 create database tech_motors;
 use tech_motors;
 
 create table usuario(
 user_id int auto_increment primary key,
-user_adm varchar(5),
-user_name varchar(60) not null,
+user_rank varchar(5) not null,
+user_name varchar(30),
+user_last_name varchar(30),
 user_nick varchar(30) not null,
-user_mail varchar(100) not null,
+user_mail varchar(100),
 user_pass varchar(20) not null,
-user_bday date not null,
+user_bday date,
 user_number varchar(20),
-user_cnh char(11),
+user_cpf char(11) not null,
 
 constraint un_user_nick unique(user_nick),
 constraint un_user_mail unique(user_mail),
-constraint un_user_cnh unique(user_cnh)
+constraint un_user_cpf unique(user_cpf)
 );
 
 create table brand(
@@ -47,7 +47,7 @@ constraint un_model_name unique(model_name),
 constraint fk_brand_id foreign key(brand_id) references brand(brand_id),
 constraint fk_oil_id foreign key(oil_id) references oil_cod(oil_id)
 );
-*/
+
 create table moto_user(
 moto_id int auto_increment primary key,
 moto_nick varchar(25) not null,
@@ -62,7 +62,7 @@ oil_id int not null,
 user_id int not null,
 
 constraint fk_user__model_id foreign key(model_id) references model(model_id),
-constraint fk_user_oil_id foreign key(oil_id) references oil_cod(oil_id),
+constraint fk_user_oil_id foreign key(oil_id) references model(oil_id),
 constraint fk_user__user_id foreign key(user_id) references usuario(user_id)
 
 );
@@ -79,3 +79,5 @@ constraint fk_service_moto_id foreign key(moto_id) references moto_user(moto_id)
 
 );
 
+
+insert into usuario(user_rank,user_nick,user_pass,user_cpf) values("owner","Owner acount","owner_1234","11122233345");
