a4ff8cda072b646a1babe43a73a33fd00d8c8c37
yann-sb <107086412+yann-sb@users.noreply.github.com>
2022-09-20 13:56:49 -0300

    php start

--- conexao.php
+++ conexao.php
@@ -9,4 +9,4 @@ $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 if($mysqli->connect_errno){
     echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_errno;
 } 
-else echo "deu boa meu bom";
+
--- index.php
+++ index.php
@@ -2,4 +2,68 @@
 
 include("conexao.php");
 
-echo "eae";
+$sql = "SELECT * FROM usuario WHERE user_id='1'"
+
+$id = $mysqli->query($sql);
+
+echo $id;
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/config.css">
+    <link rel="stylesheet" href="css/login_pg.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="block_bk row left align">
+
+            <div class="column align title block">
+                
+                <br>
+                <h2>Faça o seu login</h2>
+                <h2>entre para o nosso time</h2>
+
+                <img src="images/logo.png" id="logo">
+
+            </div>
+
+            <div class="column block align right">
+                <h1 class="text_align" style="margin-left:30px; color: aliceblue;">Acessar conta</h1>
+
+                <div class="row align">
+                    <img src="images/icons/user.png" class="icon top">
+                    <div class="in">
+                        <input type="text" placeholder="Nome de usuário ou E-mail" id="name_mail">
+                    </div>
+                </div>
+                <br><br>
+                <div class="row align">
+                    <img src="images/icons/pass.png" class="icon top">
+                    <div class="in">
+                        <input type="password" placeholder="Senha" id="pass_box">
+                        <img src="images/icons/view_pto.png" style="margin-bottom: -13px;" onclick="pass_view();">
+                    </div>
+                </div>
+                <div class="align" style="padding-left:30px">
+                    <br>
+                    <button class="but full align text_align"><p class="top">Acessar conta</p></button>
+                    <br><br>
+                    <button class="but align text_align" onclick="pag_up('create_user_pg.html');"><p class="top">Criar conta</p></button>
+                </div>
+
+            </div>
+
+    </div>
+
+
+<script src="js/js_login_1.js"></script>
+</body>
+</html>
