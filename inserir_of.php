83816e674810356acd9e96d232dcec8976951d6d
WALTER BERTOLUCI <walter.bertoluci@gmail.com>
2022-10-05 20:48:18 -0300

    add

--- inserir_of.php
+++ inserir_of.php
@@ -4,6 +4,37 @@
 
     $func = $_POST["func"];
 
+    if($func=="1"){
+
+        $nick = $_POST["name_mail"];
+        $pass = $_POST["pass_box"];
+ 
+        if($nick==null){
+            echo pag_up('index.php');
+        }elseif($pass==null){
+            echo pag_up('index.php');
+        }else{
+
+            $sql_name = "SELECT user_pass FROM usuario WHERE user_nick='$nick'";
+            $sql_mail = "SELECT user_pass FROM usuario WHERE user_mail='$nick'";
+
+            $result_name = $pdo->query($sql_name)->fetch(); 
+            $result_mail = $pdo->query($sql_mail)->fetch(); 
+            
+            if(base64_decode($result_name[0])==$pass){
+                echo pag_up('profile.html');
+            }else{
+                if(base64_decode($result_mail[0])==$pass){
+                    echo pag_up('profile.html');
+                }else{
+                    echo pag_up('index.php');
+                }
+            }
+
+    }
+
+    }
+
     if($func=="2"){
 
         $nome = $_POST["user_name"];
@@ -29,36 +60,7 @@
         echo pag_up('index.php');
     }
 
-    if($func=="1"){
 
-        $nick = $_POST["name_mail"];
-        $pass = $_POST["pass_box"];
- 
-        if($nick==null){
-            echo pag_up('index.php');
-        }elseif($pass==null){
-            echo pag_up('index.php');
-        }else{
-
-            $sql_name = "SELECT user_pass FROM usuario WHERE user_nick='$nick'";
-            $sql_mail = "SELECT user_pass FROM usuario WHERE user_mail='$nick'";
-
-            $result_name = $pdo->query($sql_name)->fetch(); 
-            $result_mail = $pdo->query($sql_mail)->fetch(); 
-            
-            if(base64_decode($result_name[0])==$pass){
-                echo "fooooiiiii";
-            }else{
-                if(base64_decode($result_mail[0])==$pass){
-                    echo "fooooiiiii";
-                }else{
-                    echo "não foi dessa vez ;)";
-                }
-            }
-
-    }
-
-    }
 
 
 
